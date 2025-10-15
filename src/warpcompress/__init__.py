793e5efd536ff2fdf74af2dd33a148deb7f74a2b
shakeeb1532 <73624484+shakeeb1532@users.noreply.github.com>
2025-10-16 10:53:58 +1100

    Update __init__.py

--- src/warpcompress/__init__.py
+++ src/warpcompress/__init__.py
@@ -1,12 +1,5 @@
 """
 WarpCompress package
 """
-from .core import (
-    CompressionManager,
-    HASH_NONE, HASH_BLAKE3, HASH_XXH64, HASH_BLAKE2B,
-)
-__all__ = [
-    "CompressionManager",
-    "HASH_NONE", "HASH_BLAKE3", "HASH_XXH64", "HASH_BLAKE2B",
-]
-__version__ = "1.0.1"
+__all__ = ["compress_file", "decompress_file"]
+__version__ = "0.1.1"
