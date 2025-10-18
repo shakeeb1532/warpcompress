eea3e9f10eec1f2cfb6adc1c4425ac61e9afef0c
shakeeb1532 <73624484+shakeeb1532@users.noreply.github.com>
2025-10-18 13:20:43 +1100

    Update __init__.py

--- src/warpcompress/__init__.py
+++ src/warpcompress/__init__.py
@@ -1,9 +1,19 @@
 """
 WarpCompress package
 """
-__all__ = ["compress_file", "decompress_file", "detect_algo_name", "__version__"]
-__version__ = "0.7.5"
+__all__ = [
+    "compress_file",
+    "decompress_file",
+    "detect_algo_name",
+    "compress_stream",
+    "decompress_stream",
+    "__version__",
+]
+
+__version__ = "0.8.0"
 
 from .core import compress_file, decompress_file, detect_algo_name
+from .stream import compress_stream, decompress_stream
+
 
 
